baea45428062fe757baad1bf1bef8bf08156d0eb
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2022-12-04 17:18:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Explorer Boilerplate
+
+## About
+
+This is a boilerplate project template For using D2KLab Explorer.
+
+## Requirements
+
+* [Docker](https://docs.docker.com/engine/)
+
+## How to run
+
+- Download this repository:
+
+```bash
+git clone https://github.com/D2KLab/explorer-boilerplate
+cd explorer-boilerplate/
+```
+
+- Open the file `.env` and edit the variables based on your environment.
+
+- Start in development mode:
+
+```bash
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
+```
+
+- Start in production mode:
+
+```bash
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
+```
+
+## License
+
+Explorer Boiletplate is [Apache licensed](https://github.com/D2KLab/explorer-boilerplate/blob/main/LICENSE).
---
+++ config.js
@@ -0,0 +1,151 @@
+module.exports = {
+  debug: typeof process !== 'undefined' && process.env.NODE_ENV === 'development',
+  metadata: {
+    title: 'D2KLab Explorer Boilerplate',
+    logo: '/images/logo.png',
+  },
+  home: {
+    hero: {
+      showHeadline: true,
+      showLogo: true,
+    },
+  },
+  footer: {
+    logo: '/images/footer.png',
+  },
+  api: {
+    endpoint: 'https://dbpedia.org/sparql',
+  },
+  search: {
+    route: 'countries',
+    allowTextSearch: true,
+    textSearchQuery: {
+      '@graph': [
+        {
+          '@id': '?id',
+          '@type': '?rdfType',
+          label: '?label',
+          representation: {
+            '@id': '?image$sample',
+            image: '?image$sample',
+          },
+        },
+      ],
+      $where: [
+        '?id a ?rdfType',
+        `VALUES ?rdfType {
+          <http://dbpedia.org/ontology/Country>
+          <http://dbpedia.org/class/yago/WikicatMemberStatesOfTheUnitedNations>
+        }`,
+        '?id dbo:capital ?capital',
+        'FILTER NOT EXISTS { ?id <http://dbpedia.org/ontology/dissolutionYear> ?yearEnd }',
+        '?id <http://www.w3.org/2000/01/rdf-schema#label> ?label',
+        '?id dbo:thumbnail ?image',
+      ],
+      $filter: ['langmatches(lang(?label), "en") || lang(?label) = ""'],
+      $limit: 5,
+      $langTag: 'hide',
+    },
+    allowImageSearch: false,
+    placeholderImage: '/images/placeholder.png',
+    languages: {
+      en: 'English',
+    },
+    graphFieldLabel: {
+      en: 'Graph',
+    },
+    defaultLanguage: 'en',
+  },
+  routes: {
+    countries: {
+      view: 'browse',
+      showInNavbar: true,
+      rdfType: 'http://dbpedia.org/ontology/Country',
+      uriBase: 'http://dbpedia.org/resource',
+      details: {
+        view: 'gallery',
+      },
+      filters: [
+        {
+          id: 'language',
+          label: 'Language',
+          isMulti: true,
+          isSortable: true,
+          query: {
+            '@graph': [
+              {
+                '@id': '?language',
+                label: '?label',
+              },
+            ],
+            $where: [
+              '?country a <http://dbpedia.org/ontology/Country>',
+              '?country <http://dbpedia.org/ontology/language> ?language',
+              '?language rdfs:label ?label',
+            ],
+            $filter: ['langmatches(lang(?label), "en") || lang(?label) = ""'],
+            $langTag: 'hide',
+          },
+          whereFunc: () => ['?id <http://dbpedia.org/ontology/language> ?language'],
+          filterFunc: (val) => `?language = <${val}>`,
+        },
+        {
+          id: 'show-only-images',
+          isOption: true,
+          whereFunc: () => [],
+        },
+      ],
+      labelFunc: (props) => props.label || props.identifier,
+      baseWhere: [
+        '?id a <http://dbpedia.org/ontology/Country>',
+        '?id a <http://dbpedia.org/class/yago/WikicatMemberStatesOfTheUnitedNations>',
+        '?id dbo:capital ?capital',
+        'FILTER NOT EXISTS { ?id <http://dbpedia.org/ontology/dissolutionYear> ?yearEnd }',
+      ],
+      query: {
+        '@graph': [
+          {
+            '@type': 'http://dbpedia.org/ontology/Country',
+            '@id': '?id',
+            label: '$<http://www.w3.org/2000/01/rdf-schema#label>$lang:en',
+            description: '$dbo:abstract$lang:en',
+            representation: {
+              '@id': '?image',
+              image: '?image',
+            },
+            capital: {
+              '@id': '?capital',
+              label: '?capitalLabel',
+            },
+          },
+        ],
+        $where: [
+          `OPTIONAL {
+            ?id dbo:capital ?capital .
+            ?capital <http://www.w3.org/2000/01/rdf-schema#label> ?capitalLabel .
+            FILTER(langmatches(lang(?capitalLabel), "en") || lang(?capitalLabel) = "")
+          }`,
+          '?id dbo:thumbnail ?image',
+        ],
+        $langTag: 'hide',
+      },
+    },
+  },
+  gallery: {
+    options: {
+      showArrows: true,
+      showStatus: true,
+      showIndicators: false,
+      infiniteLoop: false,
+      showThumbs: true,
+      autoPlay: false,
+      stopOnHover: true,
+      swipeable: true,
+      dynamicHeight: false,
+      emulateTouch: true,
+    },
+  },
+  graphs: [],
+  vocabularies: {},
+  plugins: {},
+};
